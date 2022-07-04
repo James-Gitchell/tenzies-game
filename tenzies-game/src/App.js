5eddca8a4868238efa318eabda1e14c32e34b438
James Gitchell <jamesgitchell.dev@gmail.com>
2022-07-03 23:18:23 -0400

    created die component and styled

--- tenzies-game/src/App.js
+++ tenzies-game/src/App.js
@@ -1,15 +1,28 @@
 import React from "react"
+import Die from "./components/Die"
 import './App.css';
+
 
 export default function App() {
   return(
-    <>
-      <div className="main">
-        <div className="center">
-          <div className="innerCircle"></div>
-        </div>
-      </div>
-    </>
+    
+     <main>
+       <div className="dice--container">
+         <Die value= "1" />
+         <Die value= "2" />
+         <Die value= "3" />
+         <Die value= "4" />
+         <Die value= "5" />
+         <Die value= "6" />
+         <Die value= "5" />
+         <Die value= "4" />
+         <Die value= "3" />
+         <Die value= "1" />         
+       </div>
+     </main>
+        
+      
+    
   )
 }
 
