9aa6f5921a6f066e314303e1541dbb812c140bc0
James Gitchell <jamesgitchell.dev@gmail.com>
2022-07-04 00:01:03 -0400

    created dice arr for random gen

--- tenzies-game/src/App.js
+++ tenzies-game/src/App.js
@@ -4,6 +4,18 @@ import './App.css';
 
 
 export default function App() {
+ function allNewDice() {
+      const newDice = []
+      for (let i = 0; i < 10; i++) {
+          newDice.push(Math.ceil(Math.random() * 6))
+      }
+      return newDice
+     }
+ console.log(allNewDice())
+
+  
+  
+
   return(
     
      <main>
@@ -17,7 +29,8 @@ export default function App() {
          <Die value= "5" />
          <Die value= "4" />
          <Die value= "3" />
-         <Die value= "1" />         
+         <Die value= "1" />   
+               
        </div>
      </main>
         
